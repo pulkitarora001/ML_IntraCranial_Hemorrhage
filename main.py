90097a47813c5ef0e2e3723bbd09264dc4d46dec
Pulkit Arora <77091403+pulkitarora001@users.noreply.github.com>
2024-04-01 14:03:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -55,9 +55,3 @@ def brain_stroke_pred(brain_ct_image: UploadFile):
 
     return {"Tumor_Present": status}
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT not set
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
-
